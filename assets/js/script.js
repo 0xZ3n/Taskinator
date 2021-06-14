3a44f5a1e69f2352e442b1baab75dba53a843d21
Devin Simpson <devin2806@gmail.com>
2021-06-14 13:51:44 -0400

    add ability to submit custom task and clear input form

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,14 @@ var taskFormHandler = function(event) {
   var taskNameInput = document.querySelector("input[name='task-name']").value;
   var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+  //check if input values are empty strings
+  if (!taskNameInput || !taskTypeInput) {
+    alert("You need to fill out the task form!");
+    return false;
+  }
+  //resets form for user to add new item
+  formEl.reset();
+
   // package up data as an object
   var taskDataObj = {
       name: taskNameInput,
